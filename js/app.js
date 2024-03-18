59ef1294f87fd6575a94d3bcd41bd5a889d69201
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-03-18 14:32:10 +0100

    Add: MILESTONE 1

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 //Creare un array di oggetti (membri del team)
-//creo array vuoto
-//creo funzione che crei un oggetto 
-//pusho nell'array i membri 
 
+//creo array vuoto
 const teamMembers = [];
 
+//creo funzione che crei un oggetto 
 function createMember(memberName, memberRole, memberPhoto){
     const member = {
         name: memberName,
         role: memberRole,
         photo: memberPhoto,
     }
-
+    
     return member
 }
 
+//pusho nell'array i membri 
 teamMembers.push(
     createMember('Wayne Barnett','Founder & CEO','wayne-barnett-founder-ceo.jpg'),
     createMember('Angela Caroll','Chief Editor','angela-caroll-chief-editor.jpg'),
@@ -24,3 +24,9 @@ teamMembers.push(
     createMember('Barbara Ramos','Graphic Designer','barbara-ramos-graphic-designer.jpg'),
 )
 
+
+//faccio un ciclo for per scorrere l'array teamMembers e stampo per ognuno le sue info
+for (let i=0; i < teamMembers.length; i++){
+    const teamMem = teamMembers[i];
+    console.log(teamMem.name, teamMem.role, teamMem.photo);
+}
