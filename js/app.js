fa670472841d13d5b3722c4b85c838b4f328c4b4
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-03-18 14:27:12 +0100

    Add: MILESTONE 0

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-console.log('ciao');
+//Creare un array di oggetti (membri del team)
+//creo array vuoto
+//creo funzione che crei un oggetto 
+//pusho nell'array i membri 
+
+const teamMembers = [];
+
+function createMember(memberName, memberRole, memberPhoto){
+    const member = {
+        name: memberName,
+        role: memberRole,
+        photo: memberPhoto,
+    }
+
+    return member
+}
+
+teamMembers.push(
+    createMember('Wayne Barnett','Founder & CEO','wayne-barnett-founder-ceo.jpg'),
+    createMember('Angela Caroll','Chief Editor','angela-caroll-chief-editor.jpg'),
+    createMember('Walter Gordon','Office Manager','walter-gordon-office-manager.jpg'),
+    createMember('Angela Lopez','Social Media Manager','angela-lopez-social-media-manager.jpg'),
+    createMember('Scott Estrada','Developer','scott-estrada-developer.jpg'),
+    createMember('Barbara Ramos','Graphic Designer','barbara-ramos-graphic-designer.jpg'),
+)
+
