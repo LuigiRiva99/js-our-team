e582ca1515ede1f0446ac432f59ed883b81ab360
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-03-18 15:05:38 +0100

    Add: MILESTONE 2, BONUS 1 & 2

--- js/app.js
+++ js/app.js
@@ -24,9 +24,25 @@ teamMembers.push(
     createMember('Barbara Ramos','Graphic Designer','barbara-ramos-graphic-designer.jpg'),
 )
 
+//seleziono il container nel quale mettere la card
+const rowContainer= document.querySelector('.row');
 
-//faccio un ciclo for per scorrere l'array teamMembers e stampo per ognuno le sue info
+//faccio un ciclo for per scorrere l'array teamMembers e stampo per ognuno le sue info creando card nel dom
 for (let i=0; i < teamMembers.length; i++){
     const teamMem = teamMembers[i];
     console.log(teamMem.name, teamMem.role, teamMem.photo);
+
+    //creo elemento colonna nel dom
+    const colElement = document.createElement('div');
+    colElement.className = 'col';
+    rowContainer.append(colElement);
+    
+    //inserisco le card nelle colonne
+    colElement.innerHTML= `
+    <div class="card" >
+        <img src="./img/${teamMem.photo}" alt="">
+        <h3>${teamMem.name}</h3>
+        <p>${teamMem.role}</p>
+    </div>
+    `;
 }
